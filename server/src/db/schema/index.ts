a27f97969c52dce5b0c53780d069b3e34f70143b
Aline <alinewille31@gmail.com>
2025-07-11 21:49:09 -0300

    criando integração com gemini

--- server/src/db/schema/index.ts
+++ server/src/db/schema/index.ts
@@ -1,8 +1,10 @@
 
 import {rooms} from './rooms.ts'
 import {questions} from './questions.ts'
+import { audioChunks } from './audio-chunks.ts'
 
 export const schema = {
   rooms,
-  questions
+  questions,
+  audioChunks
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -11,6 +11,8 @@ import { getRoomsRoute } from './http/routes/get-rooms.ts'
 import { createRoomsRoute } from './http/routes/create-room.ts'
 import { getRoomsQuestions } from './http/routes/get-rooms-questions.ts'
 import { createQuestionRoute } from './http/routes/create-question.ts'
+import { uploadAudioRoute } from './http/routes/upload-audio.ts'
+import {fastifyMultipart} from '@fastify/multipart'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -19,6 +21,7 @@ app.register(fastifyCors,{
     origin: 'http://localhost:5173',
 })
 
+app.register(fastifyMultipart)
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
@@ -31,5 +34,6 @@ app.register(getRoomsRoute)
 app.register(createRoomsRoute)
 app.register(getRoomsQuestions)
 app.register(createQuestionRoute)
+app.register(uploadAudioRoute)
 
 app.listen({port: env.PORT})
